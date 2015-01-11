6bfeef30163b1bc0c9c6816f1c2a836d8c6674cf
J. Gomez <alambike@gmail.com>
2015-01-11 04:29:05 +0100

    Bump version to 0.1.4

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "info@riyic.com"
 license          "Apache 2.0"
 description      "Cookbook to install ruby language and gems through rvm"
 #long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "0.1.4"
 
 ## Imprescindible en chef 11!!!
 depends "rvm"
